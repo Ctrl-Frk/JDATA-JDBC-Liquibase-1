a87c1b35ae46bfc926cc2d099b752e4761cc1ac0
Javaslavskiy <slava_bar@list.ru>
2024-02-11 23:43:53 +0300

    Initial commit

---
+++ src/main/java/baranov/viacheslav/jdatajdbcliquibase/JdataJdbcLiquibaseApplication.java
@@ -0,0 +1,11 @@
+package baranov.viacheslav.jdatajdbcliquibase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JdataJdbcLiquibaseApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(JdataJdbcLiquibaseApplication.class, args);
+    }
+}
---
+++ src/test/java/baranov/viacheslav/jdatajdbcliquibase/JdataJdbcLiquibaseApplicationTests.java
@@ -0,0 +1,13 @@
+package baranov.viacheslav.jdatajdbcliquibase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JdataJdbcLiquibaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
